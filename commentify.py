b6e5f310b3aa15a4cf1b3247c44a6b616e64a62c
Lee Raulin <leeraulin@gmail.com>
2018-05-27 13:59:43 -0400

    preserve newlines, add strip newlines option

--- commentify.py
+++ commentify.py
@@ -5,6 +5,8 @@ use-cases are removing line-breaks from copied text (ie from a PDF), and adding
 line breaks to make text suitable as code comments. 
 """
 
+# TODO: Triple comments in Python are string literals, used for docstrings.
+
 import argparse
 import pyperclip
 
@@ -14,15 +16,17 @@ END_BLOCK_COMMENT = {'py': '"""', 'js': ' */'}
 LINE_COMMENT = {'py': '# ', 'js': '// '}
 
 
-def line_comments(text, style, max=79):
+def line_comments(text, style, indent, max=79):
+    max -= indent
+    indent = ' ' * indent
     text = text.split(' ')
     new_text = ['']
-    new_text += LINE_COMMENT[style]
+    new_text += indent + LINE_COMMENT[style]
     line = 0
 
     for word in text:
         if len(new_text[line] + word) >= max:
-            new_text[line] += '\n'
+            new_text[line] += '\n' + indent
             line += 1
             new_text.append(LINE_COMMENT[style] + word)
         else:
@@ -33,26 +37,33 @@ def line_comments(text, style, max=79):
     return new_text
 
 
-def block(text, comment_style, max=79):
+def block(text, comment_style, indent, max=79):
     """Reflow text to fit max column width and add selected delimiters."""
-    text = text.replace('\n', ' ')
+    max -= indent
+    indent = indent * ' '
+    text = text.replace('\n', ' \n ')
     text = text.split(' ')
-    new_text = ['']
+    new_text = [indent]
     if comment_style:
-        new_text[0] += START_BLOCK_COMMENT[comment_style] + '\n'
+        new_text[0] += indent + START_BLOCK_COMMENT[comment_style] + '\n'
     if comment_style == "js":
         max -= 3
-        new_text[0] += ' * '
+        new_text[0] += indent + ' * '
     line = 0
 
     for word in text:
-        if len(new_text[line] + word) >= max:
+        if word == '':
+            continue
+        if len(new_text[line] + word) >= max or word == '\n':
+            if word == '\n':
+                word = ''
+            else:
+                word += ' '
             new_text[line] += '\n'
             if comment_style == "js":
                 new_text[line] += ' * '
             line += 1
             new_text.append(word)
-            new_text[line] += ' '
         else:
             new_text[line] += word + ' '
 
@@ -75,16 +86,15 @@ def main():
         '-p',
         '--python',
         action='store_true',
-        default=False,
         dest='python',
-        help='output as Python-style comments (# or """). Default block comment'
-        + 'style. Use option --line-comments (-l) for line comments.'
+        help='output as Python-style comments or docstring string literals (# '
+        + 'or """). Default docstring style. Use option --line-comments (-l) '
+        + 'for comments.'
     )
     parser.add_argument(
         '-j',
         '--javascript',
         action='store_true',
-        default=False,
         dest="javascript",
         help='output as JavaScript-style comments (// or /* */). Default block'
         + 'comment style. Use option --line-comments (-l) for line comment '
@@ -94,15 +104,17 @@ def main():
         '-l',
         '--line-comments',
         action='store_true',
-        default=False,
-        help='output as line comments',
+        dest='line',
+        help='output as line comments. Note that Python block comments are same'
+        + 'as line comments.',
     )
     parser.add_argument(
         '-b',
         '--block-comments',
-        help='output as block comments (default)',
         default=True,
-        action='store_true'
+        action='store_true',
+        dest='block',
+        help='output as block comment, or docstring string literal for Python'
     )
     parser.add_argument(
         '-m',
@@ -116,19 +128,33 @@ def main():
     parser.add_argument(
         '-r',
         '--remove',
+        action='store_true',
         help='remove line breaks from text. Incompatible with comment style '
-        + 'options',
-        action='store_true'
+        + 'options'
     )
     parser.add_argument(
         '-q',
         '--quote',
-        help='surround text with quotes',
-        action='store_true'
+        action='store_true',
+        help='surround text with quotes'
+    )
+    parser.add_argument(
+        '-i',
+        '--indent',
+        action='count',
+        help='add indent for each time option is used. Indent depnds on style.'
+        + 'Default 4, 2 for JS. Ie -jiii indents 6 spaces.'
+    )
+    parser.add_argument(
+        '-s',
+        '--strip-newlines',
+        action='store_true',
+        dest='strip',
+        help='strip newlines before reformatting'
     )
 
     args = parser.parse_args()
-    print(args)
+    indent = args.indent * 4 if args.indent else 0
 
     text = pyperclip.paste()
 
@@ -136,15 +162,20 @@ def main():
         comment_style = "py"
     elif args.javascript:
         comment_style = "js"
+        indent = int(indent / 2)
     else:
         comment_style = None
 
+    if args.strip:
+        text == text.replace('\n', ' ')
+
     if args.remove:
         pyperclip.copy(remove_line_breaks(text))
-    elif args.line_comments:
-        pyperclip.copy(line_comments(text, comment_style, args.max_lines))
-    elif args.block_comments:
-        pyperclip.copy(block(text, comment_style, args.max_lines))
+    elif args.line:
+        pyperclip.copy(line_comments(
+            text, comment_style, indent, args.max_lines))
+    elif args.block:
+        pyperclip.copy(block(text, comment_style, indent, args.max_lines))
 
 
 if __name__ == '__main__':
